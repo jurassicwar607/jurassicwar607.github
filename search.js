2fe84b599b5f76de9a28b81223c7c953c9740d2b
jurassicwar607 <56867119+jurassicwar607@users.noreply.github.com>
2019-11-01 23:40:44 +0100

    Add files via upload

--- search.js
+++ search.js
@@ -1,12 +1,12 @@
-var buscador = $("#table").datatable();
+var buscador = $("#table").DataTable();
 
 $("#input-search").keyup(function(){
-  
-  buscador.search($(this).val()).draw();
-  
-  if($("#input-search").val() ==""){
-    $(".content-search").fadeout();
+    
+    buscador.search($(this).val()).draw();
+    
+    if ($("#input-search").val() == ""){
+        $(".content-search").fadeOut(300);
     }else{
-      $(".content-search").fadein();
+        $(".content-search").fadeIn(300);
     }
 })
